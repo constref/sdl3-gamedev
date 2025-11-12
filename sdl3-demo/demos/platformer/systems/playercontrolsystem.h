688b98ff07b817773f7bf3b6a70d6a9c9768a3d5
Nick Enchev <nick@ensoft.io>
2025-11-12 08:06:53 +0200

    Starting player control system

---
+++ sdl3-demo/demos/platformer/systems/playercontrolsystem.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <system/system.h>
+#include <components/inputcomponent.h>
+#include <components/playercontrollercomponent.h
+
+class PlayerCoontrolSystem : public System<InputComponent, PlayerCoontrolSystem>
+{
+};
