c5dea3bc4b66e2be5b2fa0154fcac59b21d01966
Nick Enchev <nick@ensoft.io>
2026-03-06 06:57:00 +0200

    Started render object data

--- sdl3-demo/engine/world.h
+++ sdl3-demo/engine/world.h
@@ -26,6 +26,11 @@ public:
 		}
 	}
 
+	constexpr static size_t capacity()
+	{
+		return MaxObjects;
+	}
+
 	NodeHandle createNode()
 	{
 		assert(!freeList.empty() && "Out of object slots in pool");
